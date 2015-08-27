50b9793cd351c2b7c79ee3f6c98b2ed382850e13
zwelchcb <Zachary.Welch@careerbuilder.com>
2015-08-27 12:02:11 -0400

    goal seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,4 +2,5 @@ persona = Persona.create(name: "Anye", description: "Ember Padawan")
 
 10.times do
   persona.behaviors.create(description: Faker::Lorem.sentence)
+  persona.goals.create(description: Faker::Lorem.sentence)
 end
