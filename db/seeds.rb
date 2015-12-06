2e21479352feab8329e2f096501f3cb5f63f064c
zwelchcb <Zachary.Welch@careerbuilder.com>
2015-12-06 15:55:09 -0500

    update seed data with different personas

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,11 +4,73 @@ user = User.create!(
   password: 'password'
 )
 
-seeds = [{ avatar: 'fizzy',      
-           color: 'denim' }, 
-         { avatar: 'batman',     
-           color: 'navy' },
-         { avatar: 'super-why',  
+# Pete "The New Breed"
+
+pete = Persona.create!(
+  name: 'Pete',
+  description: "\"In my humble opinion, that's a prophetic statement.\"",
+  avatar: 'fizzy',
+  color: 'denim',
+  user: user
+)
+
+pete.behaviors.create!(
+  description: "Will do whatever it takes to get ahead"
+)
+pete.behaviors.create!(
+  description: "Puts in long hours at the office"
+)
+pete.behaviors.create!(
+  description: "Values high efficiency"
+)
+
+pete.goals.create!(
+  description: "Concerned about friends who make more money than him"
+)
+
+pete.goals.create!(
+  description: "Has clear short and long term financial goals"
+)
+
+pete.goals.create!(
+  description: "Biggest fear is becoming an irrelevant failure"
+)
+
+# Bruce "The Go-Getter"
+
+bruce = Persona.create!(
+  name: "Bruce",
+  description: "\"It's not who I am underneath, but what I do that defines me.\"",
+  avatar: 'batman',
+  color: 'navy',
+  user: user
+)
+
+bruce.behaviors.create!(
+  description: "Engages in vigilante activities"
+)
+
+bruce.behaviors.create!(
+  description: "Often uses physical force to subdue those he deems lawbreakers"
+)
+
+bruce.behaviors.create!(
+  description: "Extremely secretive and generally works alone"
+)
+
+bruce.goals.create!(
+  description: "Bring order and justice to Gotham City"
+)
+
+bruce.goals.create!(
+  description: "Turn fear against those who prey on the fearful"
+)
+
+bruce.goals.create!(
+  description: "Find the absolute best car"
+)
+
+seeds = [{ avatar: 'super-why',  
            color: 'blue' },
          { avatar: 'rigby',     
            color: 'purple' },
@@ -31,8 +93,8 @@ seeds = [{ avatar: 'fizzy',
 
 seeds.each do |seed|
   persona = Persona.create!(
-    name: 'Pervy Sage',
-    description: "\"I don't mean to brag, I don't mean to boast, but I have some hummus for these mini toast!\"",
+    name: Faker::Name.first_name,
+    description: "\"#{Faker::Hipster.sentence}\"",
     avatar: seed[:avatar],
     color: seed[:color],
     user: user
